864ae3fc243553140e9ddf334dd28b57cda80543
Eruerufusama <34217008+Eruerufusama@users.noreply.github.com>
2022-01-23 23:11:08 +0100

    Post is now visible in comments

--- src/routes/r/[slug]/comments/[slug]/[slug].svelte
+++ src/routes/r/[slug]/comments/[slug]/[slug].svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import Comments from "$lib/compontents/Comment/Comments.svelte";
+    import Post from "$lib/compontents/Post/Post.svelte";
     import { submissionStore } from "$lib/stores";
+
+    console.log($submissionStore);
 </script>
 
 <div>
     <aside>
-        Hello world
+        <Post post={$submissionStore[0].data.children[0].data} />
     </aside>
     <main>
         <Comments data={$submissionStore[1]} />
@@ -17,10 +20,14 @@
         //  Layout
             display: grid;
             place-items: center;
+            gap: 2rem;
 
+        main, aside {
+            //  Layout
+                width: min(960px, 95vw);
+        }
         main {
             //  Layout
-                width: 960px;
                 display: flex;
                 flex-direction: column;
                 gap: 1.25rem;
